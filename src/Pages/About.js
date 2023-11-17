ba5f8567b75c6c6a22d4da939eb2ccfe43f091a5
Sharuka Rajapaksha <rajapakshasharuka@gmail.com>
2023-11-17 21:05:15 +0200

    UPDATE Pages

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import {Button} from 'react-bootstrap'
 
 export default function About() {
+  const Navigate = useNavigate();
+
   return (
     <div>
         <div>
             <h1>This is About Page</h1>
         </div>
 
-
+        <Button onClick={ () => Navigate(-1)}>
+          Back to Home
+        </Button>
     </div>
   )
 }
